e55522643e63a850500a5cb0f9677bc76c899315
WilliaamKing <dimkas2001@icloud.com>
2019-08-04 22:38:57 +0300

    Updates

---
+++ precache-manifest.c2d15107a516cbac207b9cd6e7dd90c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0469aa30a584cccb7f87eef75ad486",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "7aa041cb1f1dfbe94bd3",
+    "url": "/Pokedex/static/css/main.d39e8813.chunk.css"
+  },
+  {
+    "revision": "2cb9ca5b15a5b8787969",
+    "url": "/Pokedex/static/js/2.395d60ef.chunk.js"
+  },
+  {
+    "revision": "7aa041cb1f1dfbe94bd3",
+    "url": "/Pokedex/static/js/main.9821b3c7.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  },
+  {
+    "revision": "36022bff24b374858ac069345c00c614",
+    "url": "/Pokedex/static/media/card_background.36022bff.jpg"
+  }
+]);
